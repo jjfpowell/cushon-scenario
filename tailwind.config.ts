a020a3685d698a7e208100b2bfc79d2c02c49d9b
Joseph Powell <jjfpowell@gmail.com>
2024-06-03 21:17:55 +0100

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: [
       {
         cushon: {
-          "primary": "#ee4d9b",
-          "secondary": "#efefef",
-          "accent": "#21b2e5",
-          "neutral": "#3d4451",
-          "base-100": "#efefef",
-        },
-      },
-    ],
-  },
+          primary: '#ee4d9b',
+          secondary: '#efefef',
+          accent: '#21b2e5',
+          neutral: '#efefef',
+          'base-100': '#ffffff'
+        }
+      }
+    ]
+  }
 };
 export default config;
