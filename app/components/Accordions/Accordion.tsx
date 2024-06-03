4fa61ecb630273021d791977c03a3253b2c4210f
Joseph Powell <jjfpowell@gmail.com>
2024-06-03 21:20:12 +0100

    created accordion component

---
+++ app/components/Accordions/Accordion.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface AccordionProps {
+  data: {
+    [key: string]: string;
+  };
+}
+
+const Accordion = ({ data }: AccordionProps) => {
+  return (
+    <div className="max-w-4xl place-self-center grid grid-flow-row auto-rows-max gap-y-4 py-4">
+      {Object.entries(data).map(([key, value], index) => (
+        <div key={index} className="collapse collapse-arrow bg-base-100">
+          <input type="radio" name="accordion" />
+          <div className="collapse-title text-xl font-medium">
+            {key as string}
+          </div>
+          <div className="collapse-content">
+            <p>{value as string}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
